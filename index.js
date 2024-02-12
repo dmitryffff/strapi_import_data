abdf81bcf211194d662fc310c2590d01f55d786b
Dmitry Fominenkov <dmitry.fominenkov@mail.ru>
2024-02-12 06:45:00 +0300

    switched to axios, added images downloading(disabled)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const csv = require('csv-parser')
 const fs = require('fs')
+const axios = require('axios').default;
 
 const csvFilePath = process.argv[2];
 const endpoint = process.argv[3];
@@ -11,9 +12,17 @@ const RELATION_SEPARATOR = '*';
 const IMAGE_SEPARATOR = '^';
 const PROCESS_COLUMN_SEPARATORS = [RELATION_SEPARATOR, IMAGE_SEPARATOR];
 const ARRAY_SEPARATOR = '; ';
-const EMPTY_STRING = '';
 
 const ADMIN_BEARER_TOKEN = "847e745300668130a674f20e64e13ae4c63fb9d04c0871317b086cb8878d5c485ae61ecfd7ff68db783c335a743aeb6996ba3b6f5e03611fede85f248c13d0a955ab6ddae8dbfef98ca69e50d2736fb7aabe2531c1258503b0b314d7587721cc3fcf0ee5078279eb63e43957be545d4f85294c845a33e976045e5e0e69c2148e"
+
+const strapiInstance = axios.create({
+  baseURL: 'http://localhost:1337/api',
+  headers: {
+    // 'Content-Type': 'multipart/form-data',
+    'Authorization': `Bearer ${ADMIN_BEARER_TOKEN}`
+  }
+});
+
 
 const relationData = {};
 
@@ -23,56 +32,47 @@ const getJsonFromCSV = async (csvPath) => {
   const csvData = await parseCsvToJson(csvPath);
 
   csvData.forEach(row => {
-    const jsonItemData = {};
+    const jsonData = {};
+    // for media
+    const jsonFormData = new FormData();
 
     getProcessPropertiesMatching()
       .forEach(([jsonPropName, csvColumnName]) => {
         const pureCsvColumnName = extractCsvColumnName(csvColumnName);
-        let jsonPropItemData = null;
-
         switch(true) {
           case checkIsRelationField(csvColumnName):
             const relationArray = row[pureCsvColumnName];
             getRelationCellValue(csvColumnName, relationArray)
-              .then(data => jsonPropItemData = data);
+              .then(data => jsonData[jsonPropName] = data);
             break;
           case checkIsImageField(csvColumnName):
             const imgUrl = row[pureCsvColumnName];
-            jsonPropItemData = null;
+            console.log('\x1b[31m' + 'checkIsImageField', imgUrl);
+            downloadImageBlobByUrl(imgUrl).then(({file, fileName}) => {
+              // download a file while an entry creation: https://docs.strapi.io/dev-docs/plugins/upload#upload-files-at-entry-creation
+              jsonFormData.append(`files.${jsonPropName}`, file, fileName);
+            });
             break;
           default:
-            jsonPropItemData = row[pureCsvColumnName];
+            jsonData[jsonPropName] = row[pureCsvColumnName];
         }
-
-        jsonItemData[jsonPropName] = jsonPropItemData;
       });
 
-    res.push(jsonItemData);
+      jsonFormData.append('data', JSON.stringify(jsonData));
+    res.push(jsonFormData);
   });
 
   return res;
 }
 
-const fetchCreateStrapiEntry = async (entry, endpoint) => {
-  console.log({ data: entry });
-  const response = await fetchStrapiApi(
-    endpoint,
-    'POST',
-    JSON.stringify({ data: entry })
-  );
-
-  return response.json();
-}
-
 const main = async () => {
   const posts = await getJsonFromCSV(csvFilePath);
-  console.log(posts);
 
   const res = await Promise.all(posts.slice(0, 5)
-    .map(entry => fetchCreateStrapiEntry(entry, endpoint)));
+    .map(entry => strapiInstance.post(endpoint, entry)));
   
   console.log(`All ${endpoint} created`);
-  console.log(res);
+  console.log(res.map(r => r.data));
 }
 
 // start the script
@@ -102,23 +102,6 @@ function checkIsImageField(value) {
 
 function separateArrayValues(value) {
   return value.split(ARRAY_SEPARATOR).map(v => v.trim());
-}
-
-function fetchStrapiApi(endpoint, method = 'GET', body = null) {
-  return fetch(`http://localhost:1337/api/${endpoint}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${ADMIN_BEARER_TOKEN}`
-    },
-    method,
-    body,
-  });
-}
-
-function handleValue(value) {
-  return (value !== undefined && value !== EMPTY_STRING) ? 
-    value.trim().replace(/^"(.+)"$/, '$1') 
-    : null;
 }
 
 function getProcessPropertiesMatching() {
@@ -171,4 +154,12 @@ function extractCsvColumnName(processCsvColumnName) {
     }
   });
   return res;
+}
+
+function downloadImageBlobByUrl(url) {
+  return fetch(url)
+    .then(async (res) => {
+      const file = await res.blob();
+      return {file, fileName: url.split('/').pop()}
+    })
 }
